8a167a1f4ae67d3e36093023cfa901b776d343cd
Vojtech Rozhon <rozhovoj@fit.cvut.cz>
2022-11-13 18:11:47 +0100

    support weighted graphs

--- anims.py
+++ anims.py
@@ -34,6 +34,47 @@ tree_scale = 3
 node_radius = 0.2
 arrow_width = 10
 
+
+class WeightedAnim(Scene):
+    def construct(self):
+        g = BFSGraph(
+            example_vertices,
+            example_edges,
+            layout="kamada_kawai",
+            layout_scale=tree_scale,
+            vertex_config={"radius": node_radius, "color": GRAY},  # for debugging
+            labels=False,  # for debugging
+            edge_config={"color": text_color},
+        )
+        self.play(
+            DrawBorderThenFill(g)
+        )
+
+        changes = []
+        i = 0.5
+
+        for edge in example_edges:
+            changes.append((edge, (i, 2 * UP)))
+            i += 0.4
+
+        print(changes)
+
+        g.create_edge_lengths(changes)
+
+        self.wait(5)
+
+        self.play(
+            g.show_edge_lengths(example_edges)
+        )
+
+        self.wait(5)
+
+        self.play(
+            g.hide_edge_lengths(example_edges)
+        )
+        self.wait(5)
+
+
 class BFSAnim(Scene):
     def construct(self):
         g = BFSGraph(
